35463662fbec73d6a142cce9881290c9b39b7662
ma224ip <mostafa1996@live.se>
2023-12-27 12:25:50 +0100

    done

--- UppgfitCalc/MyCalc.cs
+++ UppgfitCalc/MyCalc.cs
@@ -1,6 +1,17 @@
 using System;
 using System.Collections.Generic;
 
+// Välkomnande meddelande
+// En lista för att spara historik för räkningar
+// Användaren matar in tal och matematiska operation
+// OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
+// Ifall användaren skulle dela med 0 visa Ogiltig inmatning!
+// Lägga resultat till listan
+// Visa resultat
+// Fråga användaren om den vill visa tidigare resultat.
+// Visa tidigare resultat
+// Fråga användaren om den vill avsluta eller fortsätta.
+
 namespace UppgfitCalc
 {
     public class MyCalc
